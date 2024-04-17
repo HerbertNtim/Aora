7737e5bd888c92740c1ca027f166b27b4b60b3a0
unknown <herbertntim15@gmail.com>
2024-04-17 20:20:14 +0000

    first commit update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,7 +4,7 @@ const RootLayout = () => {
   return (
     <>
       <Stack>
-        <Stack.Screen name="index" component={index} />   
+        <Stack.Screen name="index" options={{ headerShown: false }} />   
       </Stack>
     </>
   )
