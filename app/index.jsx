05ba63516841cf881734b88d5f5404a3827c5667
unknown <herbertntim15@gmail.com>
2024-04-17 20:15:30 +0000

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl">
+          Hello 
+        </Text>
+      </View>
+  )
+}
+
+export default index
